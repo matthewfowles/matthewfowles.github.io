72434da2bbdfbac787d2ba587f7c5f4ec135dd34
Matthew Fowles <matthew.fowles@cohaesus.co.uk>
2015-01-12 17:57:45 +0000

    Fixed iOS reel bug and removed window scroll event listener in favour of RAF

--- src/js/modules/reel.js
+++ src/js/modules/reel.js
@@ -33,12 +33,17 @@ Author: Matthew Fowles
 
     reel.init = function() {
 
-        w.addEventListener('scroll', function(e) {
-            reel.getScroll();
-            reel.update();
-        });
-
         reel.getScroll();
+
+        function animate() {
+            requestAnimationFrame(function() {
+                reel.getScroll();
+                reel.update();
+                animate();
+            });
+        }
+
+        animate();
 
     };
 
@@ -94,22 +99,22 @@ Author: Matthew Fowles
 
     reel.calculate = function(scroller) {
 
-            var scrollDiff = scroller.fromPos - scroller.toPos,
-                scrollAmount = reel.scroll - scroller.fromPos,
-                propertyDiff = scroller.fromStyle - scroller.toStyle,
-                unit = propertyDiff / scrollDiff,
-                animateFrom = scroller.element.style[scroller.type.property] || 0,
-                animateTo = unit * scrollAmount;
+        var scrollDiff = scroller.fromPos - scroller.toPos,
+            scrollAmount = reel.scroll - scroller.fromPos,
+            propertyDiff = scroller.fromStyle - scroller.toStyle,
+            unit = propertyDiff / scrollDiff,
+            animateFrom = scroller.element.style[scroller.type.property] || 0,
+            animateTo = unit * scrollAmount;
 
-            if(animateTo === scroller.toStyle || animateTo === scroller.fromStyle) {
-            	return;
-            } else if (animateTo > scroller.toStyle) {
-            	reel.animate(scroller.element, scroller.type, animateFrom, scroller.toStyle);
-            } else if(animateTo < scroller.fromStyle) {
-            	reel.animate(scroller.element, scroller.type, animateFrom, scroller.fromStyle);
-            } else {
-            	reel.animate(scroller.element, scroller.type, animateFrom, animateTo);
-            }
+        if (animateTo === scroller.toStyle || animateTo === scroller.fromStyle) {
+            return;
+        } else if (animateTo > scroller.toStyle) {
+            reel.animate(scroller.element, scroller.type, animateFrom, scroller.toStyle);
+        } else if (animateTo < scroller.fromStyle) {
+            reel.animate(scroller.element, scroller.type, animateFrom, scroller.fromStyle);
+        } else {
+            reel.animate(scroller.element, scroller.type, animateFrom, animateTo);
+        }
     };
 
 
@@ -121,7 +126,7 @@ Author: Matthew Fowles
 
     reel.animate = function(element, property, from, to) {
 
-        	reel.property[property].update(element, to);
+        reel.property[property].update(element, to);
     };
 
 
@@ -174,12 +179,45 @@ Author: Matthew Fowles
         rotate: {
             property: 'transform',
             update: function(element, value) {
-            	var re = /rotate\([-\d\d]+deg\)/g;
-            		element.style.transform = 'rotate(' + value + 'deg)';
-            	
+                var re = /rotate\([-\d\d]+deg\)/g;
+                element.style.transform = 'rotate(' + value + 'deg)';
+                element.style.webkitTransform = 'rotate(' + value + 'deg)';
             }
         }
     };
+
+
+    /* 
+
+    ## Request animation polyfill
+
+    */
+
+    (function() {
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+            window.requestAnimationFrame = function(callback, element) {
+                var currTime = new Date().getTime();
+                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+                var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+
+        if (!window.cancelAnimationFrame)
+            window.cancelAnimationFrame = function(id) {
+                clearTimeout(id);
+            };
+    }());
 
 
     module.exports = reel;
