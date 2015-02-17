80a9782110d671a3c7fe864eecdc88d0558ca018
Matthew Fowles <matthew.fowles@cohaesus.co.uk>
2015-02-17 23:09:32 +0000

    Clean up and change of fonts

--- assets/canvas-component.jshint.js
+++ assets/canvas-component.jshint.js
@@ -1,4 +1,1 @@
-module('JSHint - .');
-test('canvas-component.js should pass jshint', function() { 
-  ok(true, 'canvas-component.js should pass jshint.'); 
-});
+/Users/matthewfowles/private/matthewfowles.github.io/tmp/jshinter-tmp_dest_dir-r61nSeQV.tmp/canvas-component.jshint.js
--- assets/modules/reel.jshint.js
+++
@@ -1,4 +0,0 @@
-module('JSHint - modules');
-test('modules/reel.js should pass jshint', function() { 
-  ok(true, 'modules/reel.js should pass jshint.'); 
-});
